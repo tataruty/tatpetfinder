92a9a10dd883539cd5ac4658b498c7f9949df0c0
Tetiana Usova <tataruty@outlook.com>
2022-08-08 09:45:19 +0100

    INIT

---
+++ lib/tatpetfinder.rb
@@ -0,0 +1,6 @@
+require "tatpetfinder/version"
+
+module Tatpetfinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tatpetfinder/version.rb
@@ -0,0 +1,3 @@
+module Tatpetfinder
+  VERSION = "0.1.0"
+end
